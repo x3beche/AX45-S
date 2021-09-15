6d4a6ecf6a93915dcfe9f08fca973d821de54587
x3beche <x3beche@gmail.com>
2021-09-16 02:15:59 +0300

    Engine Optimization

--- python/Engine/ax45sEngine/algorithms.py
+++ python/Engine/ax45sEngine/algorithms.py
@@ -1,28 +1,21 @@
 def f_encrypt(oW,nW,rtry):
-    own=int(rtry[0][rtry[1].index(oW)])
-    nwn=int(rtry[0][rtry[1].index(nW)])
-    if own>nwn:
-        chc=94-own+nwn
-    else:
-        chc=nwn-own
+    own,nwn=int(rtry[0][rtry[1].index(oW)]),int(rtry[0][rtry[1].index(nW)])
+    if own>nwn: chc=94-own+nwn
+    else: chc=nwn-own
     return rtry[1][chc-1]
 
 def f_decrypt(oW,nW,rtry):
-    own=int(rtry[0][rtry[1].index(oW)])
-    nwn=int(rtry[0][rtry[1].index(nW)])
-    if own+nwn>94:
-        chc=own+nwn-94
-    else:
-        chc=nwn+own
+    own,nwn=int(rtry[0][rtry[1].index(oW)]),int(rtry[0][rtry[1].index(nW)])
+    if own+nwn>94: chc=own+nwn-94
+    else: chc=nwn+own
     return rtry[1][chc-1]
 
 def keyComplier(fn):
-    fn='key'+str(fn)+'.ax'
+    fn,rtry='key'+str(fn)+'.ax',[[],[]]
     f=open(fn,'r')
     klst=f.read().replace('\n','').split('split')
     f.close()
     a=klst[0].split('axen')
-    rtry=[[],[]]
     for x in range(0,94):
         cache=klst[x].split('axen')
         rtry[0].append(cache[0])
@@ -31,20 +24,16 @@ def keyComplier(fn):
     return rtry,a
 
 def axen_algorithm(text,keyNumber):
-    a=keyComplier(keyNumber)[1]
-    rtry=keyComplier(keyNumber)[0]
+    a,rtry,final=keyComplier(keyNumber)[1],keyComplier(keyNumber)[0],''
     oW=rtry[1][ord(a[1])-32]
-    final=''
     for y in range(0,len(text)):
         oW=f_encrypt(oW,text[y],rtry)
         final=final+oW
     return final
 
 def axde_algorithm(text,keyNumber):
-    a=keyComplier(keyNumber)[1]
-    rtry=keyComplier(keyNumber)[0]
+    a,rtry,final=keyComplier(keyNumber)[1],keyComplier(keyNumber)[0],''
     oW=rtry[1][ord(a[1])-32]
-    final=''
     for y in range(0,len(text)):
         nW=text[y]
         final=final+f_decrypt(oW,nW,rtry)
