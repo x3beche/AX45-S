bf3926cd002cd245f18e3eccfe6c6583b488cb35
x3beche <x3beche@gmail.com>
2021-10-26 16:54:32 +0300

    Update keyConverterOldToNew.py

--- python/ax45sEngine/keyConverterOldToNew.py
+++ python/ax45sEngine/keyConverterOldToNew.py
@@ -1,15 +1,14 @@
 from json import dump
+from os import name,system
 
-introductions  = """Storage and operation of key files at 22.10.2021 have been improved. 
-If you have old key files, you can update your keys using the /python/ax45sEngine/keyConverterOldToNew.py program.
-*------------------------------------------------------------------------*
-Introductions : First, make sure you back up the old key file, then put the old key file in the 
-directory where this program is located and when program started enter your key number in the input section. 
-The program will update the key file."""
-print(introductions)
-
-filenum = input("Number of the key file >> ")
-
+def banner():
+    f=open('banner', 'r')
+    print(f.read())
+    f.close()
+def clear():
+    system('cls' if name == 'nt' else 'clear')
+def line():
+    print('-----------------------------------------------------')
 def keyComplier(fn):
     fn,rtry='key'+fn+'.ax',[[],[]]
     try:
@@ -27,6 +26,13 @@ def keyComplier(fn):
         print("Wrong key number!")
         exit()
 
+introductions_0 ,introductions_1 = """Storage and operation of key files at 22.10.2021 have been improved.
+If you have old key files, you can update your keys using the /python/ax45sEngine/keyConverterOldToNew.py program.""","""Introductions : First, make sure you back up the old key file, then put the old key file in the
+directory where this program is located and when program started enter your key number in the input section.
+The program will update the key file."""
+clear(),banner(),line(),print(introductions_0),line(),print(introductions_1),line()
+
+filenum = input("Number of the key file >> ")
 oldDesignKey = keyComplier(filenum)
 key, json_data, regularKey, randomFileNumber = {}, {}, [[],[]], filenum
 for x in range(0,94): regularKey[0].append(oldDesignKey[1][x]), regularKey[1].append(oldDesignKey[0][x])
